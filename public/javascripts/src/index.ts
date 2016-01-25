325647352ca0dec4a1c4b1f4b0d3a4c20e604c45
nashika <info@nashika.net>
2016-01-26 00:10:45 +0900

    ts index.js

--- public/javascripts/src/index.ts
+++ public/javascripts/src/index.ts
@@ -1,22 +1,28 @@
 // angular.js setting
 window['app'] = angular.module('App', ['ngRoute', 'ui.bootstrap', 'ngSanitize', 'ui.select']);
-app.config(['$compileProvider', function ($compileProvider) {
-        $compileProvider.aHrefSanitizationWhitelist(/^\s*(http|https|mailto|evernote):/);
-    }]);
+
+app.config(['$compileProvider', ($compileProvider) => {
+    $compileProvider.aHrefSanitizationWhitelist(/^\s*(http|https|mailto|evernote):/);
+}]);
+
 // route settings
 require('./route');
+
 // angular.js filters
 require('./filters/abbreviate');
 require('./filters/filter-by-property');
 require('./filters/object-length');
 require('./filters/order-object-by');
 require('./filters/spent-time');
+
 // angular.js services
 require('./services/data-store');
 require('./services/data-transciever');
 require('./services/progress');
+
 // angular.js directives
 require('./directives/resize');
+
 // angular.js controllers
 require('./controllers/auth-controller');
 require('./controllers/controller');
@@ -26,4 +32,3 @@ require('./controllers/notes-controller');
 require('./controllers/progress-modal-controller');
 require('./controllers/settings-controller');
 require('./controllers/timeline-controller');
-//# sourceMappingURL=index.js.map
