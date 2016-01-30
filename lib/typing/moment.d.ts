687b0a1b73a892605ca96e6b289e80610333d5cd
nashika <info@nashika.net>
2016-01-31 00:02:07 +0900

    add forked tsd repository

--- lib/typing/moment.d.ts
+++
@@ -1,8 +0,0 @@
-declare module moment {
-    export interface Moment {
-        /**
-         * @since 2.10.7+
-         */
-        isSameOrAfter:(b:MomentComparable, granularity?:string) => boolean;
-    }
-}
