dc97fcdf50b6122323801f198dc0dc5f9a7a6a39
nashika <info@nashika.net>
2016-09-18 17:25:10 +0900

    fix timeline reload bug

--- src/client/component/timeline-component.ts
+++ src/client/component/timeline-component.ts
@@ -57,51 +57,80 @@ export class TimelineComponent extends BaseComponent {
   }
 
   ready() {
+    let container: HTMLElement = <HTMLElement>this.$el.querySelector("#timeline");
+    // set working time
+    let hiddenDates: {start: moment.Moment, end: moment.Moment, repeat: string}[];
+    if (this.dataStoreService.settings && this.dataStoreService.settings["startWorkingTime"] && this.dataStoreService.settings["endWorkingTime"])
+      hiddenDates = [{
+        start: moment().subtract(1, "days").startOf("day").hour(this.dataStoreService.settings["endWorkingTime"]),
+        end: moment().startOf("day").hour(this.dataStoreService.settings["startWorkingTime"]),
+        repeat: "daily",
+      }];
+    else
+      hiddenDates = [];
+    // generate timeline object
+    this.timelineItems = new vis.DataSet();
+    this.timelineGroups = new vis.DataSet();
+    this.timeline = new vis.Timeline(container, this.timelineItems, this.timelineGroups, {
+      margin: {item: 5},
+      height: window.innerHeight - 80,
+      orientation: {axis: "both", item: "top"},
+      start: this.start,
+      end: this.end,
+      order: (a: TimelineItem, b: TimelineItem) => {
+        return a.start.getTime() - b.start.getTime();
+      },
+      hiddenDates: hiddenDates,
+    });
+    // set person data
+    if (!this.dataStoreService.settings || !this.dataStoreService.settings["persons"]) return;
+    for (let person of this.dataStoreService.settings["persons"])
+      this.timelineGroups.add({
+        id: person.name,
+        content: person.name,
+      });
+    this.timelineGroups.add({
+      id: "updated",
+      content: "Update",
+    });
+    // add events
+    this.timeline.on("rangechanged", (properties: {start: Date, end: Date}) => this.onRangeChanged(properties));
+    // reload
     this.reload();
   }
 
   reload() {
     this.dataTranscieverService.reload({start: this.start, end: this.end, getContent: true}).then(() => {
-      let container: HTMLElement = <HTMLElement>this.$el.querySelector("#timeline");
-      // set working time
-      let hiddenDates: {start: moment.Moment, end: moment.Moment, repeat: string}[];
-      if (this.dataStoreService.settings && this.dataStoreService.settings["startWorkingTime"] && this.dataStoreService.settings["endWorkingTime"])
-        hiddenDates = [{
-          start: moment().subtract(1, "days").startOf("day").hour(this.dataStoreService.settings["endWorkingTime"]),
-          end: moment().startOf("day").hour(this.dataStoreService.settings["startWorkingTime"]),
-          repeat: "daily",
-        }];
-      else
-        hiddenDates = [];
-      // generate timeline object
-      this.timelineItems = new vis.DataSet();
-      this.timelineGroups = new vis.DataSet();
-      this.timeline = new vis.Timeline(container, this.timelineItems, this.timelineGroups, {
-        margin: {item: 5},
-        height: window.innerHeight - 80,
-        orientation: {axis: "both", item: "top"},
-        start: this.start,
-        end: this.end,
-        order: (a: TimelineItem, b: TimelineItem) => {
-          return a.start.getTime() - b.start.getTime();
-        },
-        hiddenDates: hiddenDates,
-      });
-      // set person data
-      if (!this.dataStoreService.settings || !this.dataStoreService.settings["persons"]) return;
-      for (let person of this.dataStoreService.settings["persons"])
-        this.timelineGroups.add({
-          id: person.name,
-          content: person.name,
-        });
-      this.timelineGroups.add({
-        id: "updated",
-        content: "Update",
-      });
-      // add events
-      this.timeline.on("rangechanged", (properties: {start: Date, end: Date}) => this.onRangeChanged(properties));
-      // reload
-      this.onReloadEnd();
+      this.timelineItems.clear();
+      let notes: {[noteGuid: string]: NoteEntity} = {};
+      for (var noteGuid in this.dataStoreService.notes) {
+        let note: NoteEntity = this.dataStoreService.notes[noteGuid];
+        notes[note.guid] = note;
+        let timelineItem: TimelineItem = {
+          id: note.guid,
+          group: "updated",
+          content: `<a href="evernote:///view/${this.dataStoreService.user.id}/${this.dataStoreService.user.shardId}/${note.guid}/${note.guid}/" title="${note.title}">${abbreviateFilter(note.title, 40)}</a>`,
+          start: moment(note.updated).toDate(),
+          type: "point",
+        };
+        this.timelineItems.add(timelineItem);
+      }
+      for (let noteGuid in this.dataStoreService.timeLogs) {
+        let noteTimeLogs = this.dataStoreService.timeLogs[noteGuid];
+        for (var timeLogId in noteTimeLogs) {
+          let timeLog: TimeLogEntity = noteTimeLogs[timeLogId];
+          let noteTitle: string = notes[timeLog.noteGuid].title;
+          let timelineItem: TimelineItem = {
+            id: timeLog._id,
+            group: timeLog.person,
+            content: `<a href="evernote:///view/${this.dataStoreService.user["id"]}/${this.dataStoreService.user["shardId"]}/${timeLog.noteGuid}/${timeLog.noteGuid}/" title="${noteTitle} ${timeLog.comment}">${abbreviateFilter(noteTitle, 20)} ${abbreviateFilter(timeLog.comment, 20)}</a>`,
+            start: moment(timeLog.date).toDate(),
+            end: timeLog.spentTime ? moment(timeLog.date).add(timeLog.spentTime, 'minutes').toDate() : null,
+            type: timeLog.spentTime ? 'range' : 'point',
+          };
+          this.timelineItems.add(timelineItem);
+        }
+      }
     });
   }
 
@@ -112,46 +141,7 @@ export class TimelineComponent extends BaseComponent {
       return;
     if (!this.start || currentStart.isBefore(this.start)) this.start = currentStart;
     if (!this.end || currentEnd.isAfter(this.end)) this.end = currentEnd;
-    this.onReload();
-  };
-
-  onReload() {
-    this.dataTranscieverService.reload({start: this.start, end: this.end, getContent: true}).then(() => {
-      this.onReloadEnd();
-    });
-  };
-
-  onReloadEnd() {
-    this.timelineItems.clear();
-    let notes: {[noteGuid: string]: NoteEntity} = {};
-    for (var noteGuid in this.dataStoreService.notes) {
-      let note: NoteEntity = this.dataStoreService.notes[noteGuid];
-      notes[note.guid] = note;
-      let timelineItem: TimelineItem = {
-        id: note.guid,
-        group: "updated",
-        content: `<a href="evernote:///view/${this.dataStoreService.user.id}/${this.dataStoreService.user.shardId}/${note.guid}/${note.guid}/" title="${note.title}">${abbreviateFilter(note.title, 40)}</a>`,
-        start: moment(note.updated).toDate(),
-        type: "point",
-      };
-      this.timelineItems.add(timelineItem);
-    }
-    for (let noteGuid in this.dataStoreService.timeLogs) {
-      let noteTimeLogs = this.dataStoreService.timeLogs[noteGuid];
-      for (var timeLogId in noteTimeLogs) {
-        let timeLog: TimeLogEntity = noteTimeLogs[timeLogId];
-        let noteTitle: string = notes[timeLog.noteGuid].title;
-        let timelineItem: TimelineItem = {
-          id: timeLog._id,
-          group: timeLog.person,
-          content: `<a href="evernote:///view/${this.dataStoreService.user["id"]}/${this.dataStoreService.user["shardId"]}/${timeLog.noteGuid}/${timeLog.noteGuid}/" title="${noteTitle} ${timeLog.comment}">${abbreviateFilter(noteTitle, 20)} ${abbreviateFilter(timeLog.comment, 20)}</a>`,
-          start: moment(timeLog.date).toDate(),
-          end: timeLog.spentTime ? moment(timeLog.date).add(timeLog.spentTime, 'minutes').toDate() : null,
-          type: timeLog.spentTime ? 'range' : 'point',
-        };
-        this.timelineItems.add(timelineItem);
-      }
-    }
+    this.reload();
   };
 
   onResize() {
