fecd9a45970bda3db42210f40fb72bf0d78371af
nashika <info@nashika.net>
2017-03-17 15:38:01 +0900

    add vue-router rewrite navbar

--- src/client/app.ts
+++ src/client/app.ts
@@ -13,9 +13,25 @@ import './filter/spent-time.filter';
 
 import {container} from "./inversify.config";
 import Vue = require("vue");
-
+import VueRouter from "vue-router";
 let BootstrapVue = require("bootstrap-vue").default;
+Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 
+import {MenuModeComponent} from "./component/mode/menu-mode.component";
+import {TimelineModeComponent} from "./component/mode/timeline-mode.component";
+import {NotesModeComponent} from "./component/mode/notes-mode.component";
+import {ActivityModeComponent} from "./component/mode/activity-mode.component";
+import {SettingsModeComponent} from "./component/mode/settings-mode.component";
+
+const routes = [
+  {path: "/", component: MenuModeComponent},
+  {path: "/timeline", component: TimelineModeComponent},
+  {path: "/notes", component: NotesModeComponent},
+  {path: "/activity", component: ActivityModeComponent},
+  {path: "/settings", component: SettingsModeComponent},
+];
+const router = new VueRouter({routes});
+
 let AppComponent = container.get("Newable<AppComponent>");
-new (<any>AppComponent)({el: "#app"});
+new (<any>AppComponent)({router}).$mount("#app");
