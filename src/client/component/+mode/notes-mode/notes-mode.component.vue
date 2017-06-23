6fbbf42a1d3fb59c5615b49fe17e39377ce011b9
nashika <info@nashika.net>
2017-06-24 08:55:43 +0900

    adding persons.id

--- src/client/component/+mode/notes-mode/notes-mode.component.vue
+++ src/client/component/+mode/notes-mode/notes-mode.component.vue
@@ -3,24 +3,34 @@
     table.table.table-bordered.table-striped.table-hover.table-sm.table-striped
       tr
         th Title
-        th(v-for="person in existPersons") {{person}}
+        th(v-for="person in existPersons") {{person.name}}
         th Total
       tr(v-for="note in notes")
         td: a(:href="'evernote:///view/' + datastoreService.user.id + '/' + datastoreService.user.shardId + '/' + note.guid + '/' + note.guid + '/'") {{note.title}}
-        td.text-right(v-for="person in existPersons.concat(['$total'])")
-          div(v-if="notesSpentTimes[note.guid] && notesSpentTimes[note.guid][person]")
-            small ({{Math.round(notesSpentTimes[note.guid][person] / notesSpentTimes[note.guid]['$total'] * 100)}}%)&nbsp;
-            | {{notesSpentTimes[note.guid][person] | spentTime}}
-          div(v-if="notesProfits[note.guid] && notesProfits[note.guid][person]")
-            | {{Math.round(notesProfits[note.guid][person])}}
+        td.text-right(v-for="person in existPersons")
+          div(v-if="notesSpentTimes[note.guid] && notesSpentTimes[note.guid][person.id]")
+            small ({{Math.round(notesSpentTimes[note.guid][person.id] / notesSpentTimes[note.guid]['$total'] * 100)}}%)&nbsp;
+            | {{notesSpentTimes[note.guid][person.id] | spentTime}}
+          div(v-if="notesProfits[note.guid] && notesProfits[note.guid][person.id]")
+            | {{Math.round(notesProfits[note.guid][person.id])}}
+        td.text-right
+          div(v-if="notesSpentTimes[note.guid] && notesSpentTimes[note.guid]['$total']")
+            | {{notesSpentTimes[note.guid][person.id] | spentTime}}
+          div(v-if="notesProfits[note.guid] && notesProfits[note.guid]['$total']")
+            | {{Math.round(notesProfits[note.guid]['$total'])}}
       tr
         th Total
-        td.text-right(v-for="person in existPersons.concat('$total')")
+        td.text-right(v-for="person in existPersons")
           div(v-if="notesSpentTimes['$total'] && notesSpentTimes['$total'][person]")
             small ({{Math.round(notesSpentTimes['$total'][person] / notesSpentTimes['$total']['$total'] * 100)}}%)&nbsp;
             | {{notesSpentTimes['$total'][person] | spentTime}}
           div(v-if="notesProfits['$total'] && notesProfits['$total'][person]")
             | {{Math.round(notesProfits['$total'][person])}}
+        td.text-right
+          div(v-if="notesSpentTimes['$total'] && notesSpentTimes['$total']['$total']")
+            | {{notesSpentTimes['$total']['$total'] | spentTime}}
+          div(v-if="notesProfits['$total'] && notesProfits['$total'][person]")
+            | {{Math.round(notesProfits['$total']['$total'])}}
 </template>
 
 <script lang="ts" src="./notes-mode.component.ts"></script>
