48b5bcb4f46373903ab9c31ae6f1ed4dbe90e63a
nashika <info@nashika.net>
2017-06-29 07:30:19 +0900

    remove app-filter-navigation

--- src/client/component/navigation/navigation.component.vue
+++ src/client/component/navigation/navigation.component.vue
@@ -4,20 +4,18 @@
       i.fa(class="fa-" + icon)
       span.hidden-xs-down &nbsp;#{label}
 
-  div
-    b-navbar(toggleable, type="inverse", toggle-breakpoint="xl", variant="inverse", fixed="top")
-      b-collapse(is-nav, id="nav-collapse")
-        b-nav.mr-auto(is-nav-bar)
-          +item("/timeline", "clock-o", "Timeline")
-          +item("/notes", "files-o", "Notes")
-          +item("/activity", "history", "Activity")
-        b-button.mr-3(variant="outline-secondary", @click="$root.$emit('show::modal', 'filter-modal')")
-          i.fa.fa-filter
-          span.hidden-sm-down &nbsp;Filter
-        b-button(variant="outline-primary", @click="reload()")
-          i.fa.fa-refresh
-          span.hidden-sm-down &nbsp;Reload
-    app-filter-navigation
+  b-navbar(toggleable, type="inverse", toggle-breakpoint="xl", variant="inverse", fixed="top")
+    b-collapse(is-nav, id="nav-collapse")
+      b-nav.mr-auto(is-nav-bar)
+        +item("/timeline", "clock-o", "Timeline")
+        +item("/notes", "files-o", "Notes")
+        +item("/activity", "history", "Activity")
+      b-button.mr-3(variant="outline-secondary", @click="$root.$emit('show::modal', 'filter-modal')")
+        i.fa.fa-filter
+        span.hidden-sm-down &nbsp;Filter
+      b-button(variant="outline-primary", @click="reload()")
+        i.fa.fa-refresh
+        span.hidden-sm-down &nbsp;Reload
 </template>
 
 <script lang="ts" src="./navigation.component.ts"></script>
