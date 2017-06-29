4a10db47b660f7e2aa9abcce30f4138bdbc57262
nashika <info@nashika.net>
2017-06-29 11:31:29 +0900

    create person-modal ui

--- src/client/component/+modal/person-modal/person-modal.component.ts
+++ src/client/component/+modal/person-modal/person-modal.component.ts
@@ -1,7 +1,34 @@
 import Component from "vue-class-component";
 
 import BaseComponent from "../../base.component";
+import {configLoader, IPersonConfig} from "../../../../common/util/config-loader";
+import {DatastoreService} from "../../../service/datastore.service";
+import {container} from "../../../inversify.config";
+import {RequestService} from "../../../service/request.service";
 
 @Component({})
 export default class PersonModalComponent extends BaseComponent {
+
+  datastoreService: DatastoreService = container.get(DatastoreService);
+  requestService: RequestService = container.get(RequestService);
+
+  persons: IPersonConfig[] = configLoader.app.persons;
+  currentPersonId: number = 0;
+
+  fields = {
+    id: {label: "ID", sortable: true},
+    name: {label: "Name", sortable: true},
+    action: {label: "Action"},
+  };
+
+  async mounted(): Promise<void> {
+    this.currentPersonId = this.datastoreService.currentPersonId;
+  }
+
+  async select(id: number): Promise<void> {
+    this.currentPersonId = id;
+    this.datastoreService.currentPersonId = id;
+    await this.requestService.saveSession("currentPersonId", id);
+  }
+
 }
--- src/client/component/+modal/person-modal/person-modal.component.vue
+++ src/client/component/+modal/person-modal/person-modal.component.vue
@@ -1,5 +1,11 @@
 <template lang="pug">
   b-modal(id="person-modal", title="Person")
+    b-table(striped, hover, :items="persons", :fields="fields")
+      template(slot="action", scope="item")
+        template(v-if="currentPersonId == item.item.id")
+          b-button(block, variant="danger") Selected
+        template(v-else)
+          b-button(block, variant="secondary", @click="select(item.item.id)") Select
 </template>
 
 <script lang="ts" src="./person-modal.component.ts"></script>
