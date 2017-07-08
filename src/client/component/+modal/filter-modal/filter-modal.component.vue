853ff8faa5135479a35afe71617ac8d86ca6c4c2
nashika <info@nashika.net>
2017-07-09 08:14:39 +0900

    add scrollbar to filter modal

--- src/client/component/+modal/filter-modal/filter-modal.component.vue
+++ src/client/component/+modal/filter-modal/filter-modal.component.vue
@@ -13,14 +13,23 @@
       b-button(variant="danger", size="sm", @click="reParse()") #[span.fa.fa-industry] Re Parse
     .mb-3
       b-form-checkbox(:checked="!!stacks", @change="toggleStack()") Filter by stack
-      b-table(v-if="stacks", :items="stacks", :fields="stackFields", striped, small)
-        template(slot="selected", scope="data")
-          b-form-checkbox(:checked="data.item.selected", @change="toggleStackItem(data.item)")
+      .scroll(v-if="stacks")
+        b-table(:items="stacks", :fields="stackFields", striped, small)
+          template(slot="selected", scope="data")
+            b-form-checkbox(:checked="data.item.selected", @change="toggleStackItem(data.item)")
     .mb-3
       b-form-checkbox(:checked="!!notebooks", @change="toggleNotebook()") Filter by notebook
-      b-table(v-if="notebooks", :items="notebooks", :fields="notebookFields", striped, small)
-        template(slot="selected", scope="data")
-          b-form-checkbox(:checked="data.item.selected", @change="toggleNotebookItem(data.item)")
+      .scroll(v-if="notebooks")
+        b-table(:items="notebooks", :fields="notebookFields", striped, small)
+          template(slot="selected", scope="data")
+            b-form-checkbox(:checked="data.item.selected", @change="toggleNotebookItem(data.item)")
 </template>
+
+<style lang="scss">
+  .scroll {
+    overflow-y: auto;
+    max-height: 200px;
+  }
+</style>
 
 <script lang="ts" src="./filter-modal.component.ts"></script>
