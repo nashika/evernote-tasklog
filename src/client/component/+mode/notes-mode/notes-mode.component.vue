dc0f7c8227a29f83b0c0948f8439189141b91d0c
nashika <info@nashika.net>
2017-07-17 06:51:05 +0900

    add profit per hour

--- src/client/component/+mode/notes-mode/notes-mode.component.vue
+++ src/client/component/+mode/notes-mode/notes-mode.component.vue
@@ -8,27 +8,32 @@
       template(slot="title", scope="data")
         a(:href="'evernote:///view/' + datastoreService.$vm.user.id + '/' + datastoreService.$vm.user.shardId + '/' + data.item.guid + '/' + data.item.guid + '/'") {{data.item.title}}
       template(v-for="person in existPersons", :slot="'person-' + person.id", scope="data")
-        .text-right(v-if="notesSpentTimes[data.item.guid] && notesSpentTimes[data.item.guid][person.id]")
-          small ({{Math.round(notesSpentTimes[data.item.guid][person.id] / notesSpentTimes[data.item.guid]['$total'] * 100)}}%)&nbsp;
-          | {{notesSpentTimes[data.item.guid][person.id] | spentTime}}
-        .text-right(v-if="notesProfits[data.item.guid] && notesProfits[data.item.guid][person.id]")
-          | {{Math.round(notesProfits[data.item.guid][person.id])}}
+        .text-right(v-if="lodash.get(notesSpentTimes, [data.item.guid, '$' + person.id])")
+          small ({{Math.round(notesSpentTimes[data.item.guid]['$' + person.id] / notesSpentTimes[data.item.guid]['$total'] * 100)}}%)&nbsp;
+          | {{notesSpentTimes[data.item.guid]['$' + person.id] | spentTime}}
+        .text-right(v-if="lodash.get(notesProfits, [data.item.guid, '$' + person.id])")
+          | {{Math.round(notesProfits[data.item.guid]['$' + person.id])}}
       template(slot="total", scope="data")
-        .text-right(v-if="notesSpentTimes[data.item.guid] && notesSpentTimes[data.item.guid]['$total']")
+        .text-right(v-if="lodash.get(notesSpentTimes, [data.item.guid, '$total'])")
           | {{notesSpentTimes[data.item.guid]['$total'] | spentTime}}
-        .text-right(v-if="notesProfits[data.item.guid] && notesProfits[data.item.guid]['$total']")
+        .text-right(v-if="lodash.get(notesProfits, [data.item.guid, '$total'])")
+          small(v-if="lodash.get(notesProfits, [data.item.guid, '$total']) && lodash.get(notesSpentTimes, [data.item.guid, '$total'])")
+            | ({{Math.round(notesProfits[data.item.guid]['$total'] / notesSpentTimes[data.item.guid]['$total'] * 60)}}/h)&nbsp;
           | {{Math.round(notesProfits[data.item.guid]['$total'])}}
+      template(slot="profitPerHour", scope="data")
       template(slot="FOOT_title", scope="data") Total
       template(v-for="person in existPersons", :slot="'FOOT_person-' + person.id", scope="data")
-        .text-right(v-if="notesSpentTimes['$total'] && notesSpentTimes['$total'][person.id]")
-          small ({{Math.round(notesSpentTimes['$total'][person.id] / notesSpentTimes['$total']['$total'] * 100)}}%)&nbsp;
-          | {{notesSpentTimes['$total'][person.id] | spentTime}}
-        .text-right(v-if="notesProfits['$total'] && notesProfits['$total'][person.id]")
-          | {{Math.round(notesProfits['$total'][person.id])}}
+        .text-right(v-if="lodash.get(notesSpentTimes, ['$total', '$' + person.id])")
+          small ({{Math.round(notesSpentTimes['$total']['$' + person.id] / notesSpentTimes['$total']['$total'] * 100)}}%)&nbsp;
+          | {{notesSpentTimes['$total']['$' + person.id] | spentTime}}
+        .text-right(v-if="lodash.get(notesProfits, ['$total', '$' + person.id])")
+          | {{Math.round(notesProfits['$total']['$' + person.id])}}
       template(slot="FOOT_total", scope="data")
-        .text-right(v-if="notesSpentTimes['$total'] && notesSpentTimes['$total']['$total']")
+        .text-right(v-if="lodash.get(notesSpentTimes, ['$total', '$total'])")
           | {{notesSpentTimes['$total']['$total'] | spentTime}}
-        .text-right(v-if="notesProfits['$total'] && notesProfits['$total']['$total']")
+        .text-right(v-if="lodash.get(notesProfits, ['$total', '$total'])")
+          small(v-if="lodash.get(notesProfits, ['$total', '$total']) && lodash.get(notesSpentTimes, ['$total', '$total'])")
+            | ({{Math.round(notesProfits['$total']['$total'] / notesSpentTimes['$total']['$total'] * 60)}}/h)&nbsp;
           | {{Math.round(notesProfits['$total']['$total'])}}
     b-modal(id="menu-modal", title="Menu", ok-only, @hidden="reload()")
       .h6 Filter profit type
