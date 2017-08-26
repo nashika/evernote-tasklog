a1aca9da23854993b89b66b2e80fb92c307979d2
nashika <info@nashika.net>
2017-08-27 07:10:26 +0900

    fix navigation-component html classes

--- src/client/component/navigation/navigation.component.vue
+++ src/client/component/navigation/navigation.component.vue
@@ -2,7 +2,7 @@
   mixin item(to, icon, label)
     b-nav-item(to=to)
       i.fa(class="fa-" + icon)
-      span.hidden-xs-down &nbsp;#{label}
+      | &nbsp;#{label}
 
   b-navbar.d-print-none(toggleable="sm", type="dark", variant="dark", fixed="top")
     b-nav-toggle(target="nav-collapse")
@@ -12,9 +12,9 @@
         +item("/timeline", "clock-o", "Timeline")
         +item("/notes", "files-o", "Notes")
         +item("/activity", "history", "Activity")
-      b-button(:variant="datastoreService.$vm.currentPersonId ? 'warning' : 'outline-warning'", @click="$root.$emit('show::modal', 'person-modal')")
-        i.fa.fa-user
-        span.hidden-sm-down &nbsp;{{personLabel}}
+    b-button(:variant="datastoreService.$vm.currentPersonId ? 'warning' : 'outline-warning'", @click="$root.$emit('show::modal', 'person-modal')")
+      i.fa.fa-user
+      span.d-none.d-sm-inline &nbsp;{{personLabel}}
 </template>
 
 <script lang="ts" src="./navigation.component.ts"></script>
