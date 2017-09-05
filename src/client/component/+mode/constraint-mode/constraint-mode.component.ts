a63d26ceae3762286aaeeada7ea6a56fe25ac25e
nashika <info@nashika.net>
2017-09-05 19:54:44 +0900

    creating constraint-mode view

--- src/client/component/+mode/constraint-mode/constraint-mode.component.ts
+++ src/client/component/+mode/constraint-mode/constraint-mode.component.ts
@@ -1,13 +1,32 @@
 import Component from "vue-class-component";
+import * as _ from "lodash";
 
 import BaseComponent from "../../base.component";
 import {DatastoreService} from "../../../service/datastore.service";
 import {container} from "../../../inversify.config";
+import {RequestService} from "../../../service/request.service";
+import {ConstraintResultEntity} from "../../../../common/entity/constraint-result.entity";
+import {configLoader} from "../../../../common/util/config-loader";
+import {NoteEntity} from "../../../../common/entity/note.entity";
+
+interface IConstraintResultRecord {
+  noteGuid: string;
+  constraintId: number;
+  constraintLabel: string;
+}
 
 @Component({})
 export default class ConstraintModeComponent extends BaseComponent {
 
+  requestService: RequestService = container.get(RequestService);
   datastoreService: DatastoreService = container.get(DatastoreService);
+
+  records: IConstraintResultRecord[] = [];
+
+  fields = {
+    noteGuid: {label: "Note guid", sortable: true},
+    constraintLabel: {label: "Constraint", sortable: true},
+  };
 
   async mounted(): Promise<void> {
     await super.mounted();
@@ -15,7 +34,17 @@ export default class ConstraintModeComponent extends BaseComponent {
   }
 
   async reload(): Promise<void> {
-
+    let constraintResults = await this.requestService.find<ConstraintResultEntity>(ConstraintResultEntity, {});
+    let noteGuids: string[] = _(constraintResults).map(constraintResult => constraintResult.noteGuid).uniq().value();
+    let notes = await this.requestService.find<NoteEntity>(NoteEntity, {where: {guid: noteGuids}});
+    this.records = [];
+    for (let constraintResult of constraintResults) {
+      this.records.push({
+        noteGuid: constraintResult.noteGuid,
+        constraintId: constraintResult.constraintId,
+        constraintLabel: _.find(configLoader.app.constraints, {id: constraintResult.constraintId}).label,
+      });
+    }
   }
 
 }
--- src/client/component/+mode/constraint-mode/constraint-mode.component.vue
+++ src/client/component/+mode/constraint-mode/constraint-mode.component.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
   section#constraint-mode.p-3
+    b-table(bordered, striped, hover, small, responsive, head-variant="inverse", foot-clone, foot-variant="default",
+    :fields="fields", :items="records")
 </template>
 
 <script lang="ts" src="./constraint-mode.component.ts"></script>
