260d20b6483c183f060233dbed546c4fb5a41e26
nashika <info@nashika.net>
2020-03-27 17:48:53 +0900

    pugの導入

--- pages/index.vue
+++ pages/index.vue
@@ -1,27 +1,13 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
+<template lang="pug">
+  .container
+    div
+      logo
+      h1.title
         evernote-tasklog
-      </h1>
-      <h2 class="subtitle">
-        My swell Nuxt.js project
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+      h2.subtitle My swell Nuxt.js project
+      .links
+        a.button--green(href="https://nuxtjs.org/", target="_blank") Documentation
+        a.button--grey(href="https://github.com/nuxt/nuxt.js", target="_blank") GitHub
 </template>
 
 <script lang="ts">
