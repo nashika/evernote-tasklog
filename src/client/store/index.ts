f4f9fc5d6f37aa948717b6a9b81cedfa72e9018c
nashika <info@nashika.net>
2020-04-27 12:07:23 +0900

    vuexのファイルを最小限まで一旦減らした

--- src/client/store/index.ts
+++
@@ -1,5 +0,0 @@
-import { Store } from "vuex";
-import { initialiseStores } from "~/src/client/utils/store-accessor";
-const initializer = (store: Store<any>) => initialiseStores(store);
-export const plugins = [initializer];
-export * from "~/src/client/utils/store-accessor";
