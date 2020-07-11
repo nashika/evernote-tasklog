20141769779805f9d8c59ec1031efcf04a75485c
nashika <info@nashika.net>
2020-07-11 16:11:34 +0900

    ProgressModalを移植

--- src/client/component/+modal/progress-modal/progress-modal.component.vue
+++
@@ -1,9 +0,0 @@
-<template lang="pug">
-  b-modal(id="progress", :visible="progressService.isActive", title="Loading", ref="modal", size="sm",
-  no-fade, no-close-on-backdrop, no-close-on-esc, hide-header, hide-footer)
-    b-progress(:value="progressService.percentage", variant="primary", striped)
-    .text-center {{progressService.percentage}}% ({{progressService.completeCount}} / {{progressService.allCount}})
-    .text-left {{progressService.message}}
-</template>
-
-<script lang="ts" src="./progress-modal.component.ts"></script>
