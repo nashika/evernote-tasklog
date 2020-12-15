ee36ec1a2740529c3c2797f92664720042569b8a
nashika <info@nashika.net>
2020-12-15 17:25:23 +0900

    phpstormのアップデートと、プロジェクト配置場所変更による修正

--- src/iko/iko.sql
+++ src/iko/iko.sql
@@ -1,4 +1,4 @@
-ATTACH "C:\Users\rokuro\Documents\workspace\evernote-tasklog-nuxt\db\database_old.db" AS old;
+ATTACH "C:\Users\info\workspace\evernote-tasklog\db\database_old.db" AS old;
 
 INSERT INTO archive_note
 SELECT * FROM old.archiveNotes;
